49e64247dd1181808ff9b2e6cf972b629ff319e2
Bagas Nanda <bagasnanda@gmail.com>
2021-12-14 15:04:45 +0700

    up bash command

--- .custom_command.sh
+++ .custom_command.sh
@@ -1,4 +1,4 @@
 function run_py_codes() {
- python3 ./just_camera_capture-copy.py
- python3 ./just_video_capture-copy.py
+ sudo python3 ./just_camera_capture-copy.py
+ sudo python3 ./just_video_capture-copy.py
 }
