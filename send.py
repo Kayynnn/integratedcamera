37e18c4b5db64ab6e9568ec897ed375b07f0f93e
farhanmidin <farhand.hamdani@gmail.com>
2021-12-16 14:19:13 +0700

    Update sending code

--- send.py
+++ send.py
@@ -8,13 +8,12 @@ password = 'b193a4a95ef9fb64'
 while True:
     if os.path.isfile('queue.txt'):
         with open('queue.txt') as f:
-            lines = f.readlines()
-                       
-        ftp = ftplib.FTP(server, user, password)
-        for z in lines:
-            print(z.strip())
-            if os.path.isfile(z.strip()):     
-             f = open(z, 'rb')
-             ftp.storbinary('STOR '+imgname[z], f)
-        ftp.quit()
-        os.remove('queue.txt')
+            lines = f.readlines()    
+            ftp = ftplib.FTP(server, user, password)
+            for z in lines:
+                imgname = z.strip() 
+                if os.path.isfile(imgname):     
+                    img = open(imgname, 'rb')
+                    ftp.storbinary('STOR '+imgname, img)
+            ftp.quit()
+            os.remove('queue.txt')
